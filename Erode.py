1652fb7ff3a8e4f80f476ede454c91e0f789f188
Alexander Korner <r.edna@live.se>
2021-11-24 11:56:29 +0100

    Tested gradientfunction and started on Erode function

--- Erode.py
+++ Erode.py
@@ -1,4 +1,42 @@
+import random
+import numpy as np
 
-import matplotlib.pyplot as plt
+erosionRadius = 3
+inertia = 0.1
+sedimentCapacityFactor = 4
+minSedimentCapacity = 0.1
+erodeSpeed = 0.3
+depositSpeed = 0.4
+evaporateSpeed = 0.1
+gravity = 4
+dropletLifetime = 30
+initialWaterVolume = 1
+initialSpeed = 1
 
-def Erode(numIterations,map)
+def Erode(numDroplets,heightMap):
+    #Assuming heightMap is a numpyArray
+    mapSize = heightMap.shape[0]
+
+    for i in range(numIterations):
+        posX = mapSize*random.random()
+        posY = mapSize*random.random()
+        speed = initialSpeed
+        water = initialWaterVolume
+
+        dirX = 0
+        dirY = 0
+
+        for j in range(dropletLifetime):
+            mapIndexX = int(posX)
+            mapIndexY = int(posY)
+
+            #Calculate droplet's offset inside the cell (0,0) = at NW node, (1,1) = at SE node
+            offsetX = posX-mapIndexX
+            offsetY = posY-mapIndexY
+
+            
+
+
+
+
+
--- GradientTest.py
+++ GradientTest.py
@@ -1,16 +1,16 @@
 # Did some testing with gradients but not quite sure how to translate SebLagues C# code for the gradient
 import math
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 
 z = np.array([[x**2 + y**2 for x in range(20)] for y in range(20)])
 x, y = np.meshgrid(range(z.shape[0]), range(z.shape[1]))
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z)
-plt.title('z as 3d height map')
-plt.show()
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d')
+#ax.plot_surface(x, y, z)
+#plt.title('z as 3d height map')
+#plt.show()
 inertia=0.01
 dropletxpos=17.1
 dropletypos=15.9
@@ -31,4 +31,6 @@ def returngradient(map,xpos,ypos):
 result=returngradient(z,dropletxpos,dropletypos) #seems to work but changeing xpos ypox to different decimal (same integer) does not change the result so might be unnecessary
 print(result)
 #ignore rest
+
+# Different decimals give diffrent height, so it is necessary
 
