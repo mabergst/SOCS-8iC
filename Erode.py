e27569f4ccba937c8869b3f1a35addd5b040fd52
Alexander Korner <r.edna@live.se>
2021-11-24 14:44:06 +0100

    Continued on Erode

--- Erode.py
+++ Erode.py
@@ -1,5 +1,8 @@
+import math
 import random
 import numpy as np
+
+from GradientTest import returngradient
 
 erosionRadius = 3
 inertia = 0.1
@@ -17,11 +20,12 @@ def Erode(numDroplets,heightMap):
     #Assuming heightMap is a numpyArray
     mapSize = heightMap.shape[0]
 
-    for i in range(numIterations):
+    for i in range(numDroplets):
         posX = mapSize*random.random()
         posY = mapSize*random.random()
         speed = initialSpeed
         water = initialWaterVolume
+        sediment = 0
 
         dirX = 0
         dirY = 0
@@ -34,8 +38,53 @@ def Erode(numDroplets,heightMap):
             offsetX = posX-mapIndexX
             offsetY = posY-mapIndexY
 
-            
+            #Get gradient and height then normalize and move droplet
+            gradientAndHeight = returngradient(heightMap,posX,posY)
 
+            dirX = (dirX * inertia - gradientAndHeight[0] * (1 - inertia))
+            dirY = (dirY * inertia - gradientAndHeight[1]* (1 - inertia))
+
+            dirLength = math.sqrt(dirX*dirX+dirY*dirY)
+            if dirLength != 0:
+                dirX = dirX/dirLength
+                dirY = dirY/dirLength
+
+            posX = posX + dirX
+            posY = posY + dirY
+
+            if dirLength == 0 or posX < 0 or posX >= mapSize - 1 or posY < 0 or posY >= mapSize - 1:
+                    break
+            
+            #Calculate change in height
+            deltaHeight = returngradient(heightMap,posX,posY)[2]-gradientAndHeight[2]
+
+            # Calculates the droplet capacity, just took this from lague, idk the derivation
+            sedimentCapacity = max(-deltaHeight * speed * water * sedimentCapacityFactor, minSedimentCapacity)
+            
+            if sediment > sedimentCapacity or deltaHeight > 0:
+
+                #If uphill, try to fill hole
+                if deltaHeight > 0:
+                    deposit = min(deltaHeight,sediment)
+                else:
+                    deposit = (sediment - sedimentCapacity) * depositSpeed
+            
+                #Bilinear interpolation between the four nodes
+                heightMap[mapIndexX,mapIndexY] += deposit * (1 - offsetX) * (1 - offsetY)
+                heightMap[mapIndexX+1,mapIndexY] += deposit * offsetX * (1 - offsetY)
+                heightMap[mapIndexX,mapIndexY+1] += deposit * (1 - offsetX) * offsetY
+                heightMap[mapIndexX+1,mapIndexY+1] += deposit * offsetX * offsetY
+            else:
+
+                #Dont remove more than deltaHeight
+                erosion = min((sedimentCapacity - sediment) * erodeSpeed, -deltaHeight)
+
+
+
+
+
+
+            
 
 
 
